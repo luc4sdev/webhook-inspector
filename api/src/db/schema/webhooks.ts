48d9a5c40aceed89a6a669a8bb2ad0288d89eda0
Lucas Pereira França <lucas.pfranca01@gmail.com>
2026-01-03 17:20:29 -0300

    feat: create api structure with docker, drizzle and fastify

---
+++ api/src/db/schema/webhooks.ts
@@ -0,0 +1,15 @@
+import { pgTable, text, integer, jsonb, timestamp } from "drizzle-orm/pg-core";
+import { uuidv7 } from 'uuidv7'
+export const webhooks = pgTable('webhooks', {
+    id: text().primaryKey().$defaultFn(() => uuidv7()),
+    method: text().notNull(),
+    pathName: text().notNull(),
+    ip: text().notNull(),
+    statusCode: integer().notNull().default(200),
+    contentType: text(),
+    contextLenght: integer(),
+    queryParams: jsonb().$type<Record<string, string>>(),
+    headers: jsonb().$type<Record<string, string>>().notNull(),
+    body: text(),
+    createdAt: timestamp().notNull().defaultNow(),
+})
---
+++ api/src/server.ts
@@ -0,0 +1,44 @@
+import { fastify } from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  type ZodTypeProvider
+} from 'fastify-type-provider-zod'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifyCors } from '@fastify/cors'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+import { listWebhooks } from './routes/list-webhooks'
+import { env } from './env'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+})
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Webhook Inspector API',
+      description: 'API documentation for Webhook Inspector',
+      version: '1.0.0'
+    }
+  },
+  transform: jsonSchemaTransform
+})
+
+app.register(ScalarApiReference, {
+  routePrefix: '/docs'
+})
+
+app.register(listWebhooks)
+
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP server running!')
+  console.log(`Swagger docs available at http://localhost:${env.PORT}/docs`)
+})
